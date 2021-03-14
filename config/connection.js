b860c366723effff0dcd60d59df9e7ae517eb984
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-14 20:57:13 +0000

    Adding comments

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
+// requiring mysql npm 
 const mysql = require('mysql');
+// requiring dotenv npm 
 require('dotenv').config();
 
+// Creating the connection to the database
 const connection = mysql.createConnection({
     multipleStatements: true,
     host: process.env.DB_HOST,
