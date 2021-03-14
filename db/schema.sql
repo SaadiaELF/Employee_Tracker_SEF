d72c2020762f3cfc3b90b69d98aab582ef1a4f76
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-14 20:58:45 +0000

    Updating db

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,12 @@ CREATE TABLE role (
 id INTEGER(10) AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(20,2) NOT NULL,
-department_id  INTEGER(10) NOT NULL, 
-PRIMARY KEY (id)
+department_id  INTEGER(10) NOT NULL DEFAULT 1, 
+PRIMARY KEY (id),
+INDEX (department_id),
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -21,5 +25,10 @@ first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id  INTEGER(10) NOT NULL, 
 manager_id  INTEGER(10), 
-PRIMARY KEY (id)
+PRIMARY KEY (id),
+INDEX (role_id),
+INDEX (manager_id),
+FOREIGN KEY (role_id)
+REFERENCES role(id)
+ON DELETE CASCADE
 );
